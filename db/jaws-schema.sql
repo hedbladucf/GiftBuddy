4a56a206a93029b8b09e4aa92972849d5d380481
Michelle Hettinger <Michelle Hettinger>
2016-09-29 23:30:09 -0400

    created jaws db tables and inserts

---
+++ db/jaws-schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE `p2o40rwbj6zuxy26`.`users_groups`;
+DROP TABLE `p2o40rwbj6zuxy26`.`users`;
+DROP TABLE `p2o40rwbj6zuxy26`.`groups`;
+
+
+use p2o40rwbj6zuxy26;
+
+CREATE TABLE `p2o40rwbj6zuxy26`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `full_name` VARCHAR(99) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+  CREATE TABLE `p2o40rwbj6zuxy26`.`groups` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_name` VARCHAR(45) NOT NULL,
+  `admin_user_id` INT NOT NULL,
+  `dollar_amount` INT NOT NULL,
+  `active` TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`));
+
+  CREATE TABLE `p2o40rwbj6zuxy26`.`users_groups` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `users_id` INT NOT NULL,
+  `groups_id` INT NOT NULL,
+  `assigned_user_id` INT NOT NULL DEFAULT 0,
+  `role` VARCHAR(45) NOT NULL DEFAULT "user",
+  `sent` TINYINT NOT NULL DEFAULT 0,
+  `received` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`users_id`) REFERENCES users(`id`),
+  FOREIGN KEY (`groups_id`) REFERENCES groups(`id`));
---
+++ db/jaws-seeds.sql
@@ -0,0 +1,31 @@
+USE p2o40rwbj6zuxy26;
+
+-- When a user signs up  
+  
+  INSERT INTO users (full_name, address, email, password)
+  VALUES ("Michelle Hettinger", "513 Oakwood Court, 32714", "mellowmichellehet@gmail.com", "michelle41989");
+  
+  INSERT INTO users (full_name, address, email, password)
+  VALUES ("Oscar Hedblad", "something", "something@something", "something");
+ 
+  INSERT INTO users (full_name, address, email, password)
+  VALUES ("Jorge Rivas", "something", "something@something", "something");
+  
+  
+-- When a group is created. The creator is assigned as admin
+  
+  INSERT INTO groups (group_name, admin_user_id, dollar_amount)
+  VALUES ("Original Gift Buddies", 1, 50);
+
+  INSERT INTO users_groups (users_id, groups_id, role)
+  VALUES (1, 1, "admin");
+
+
+-- When a user is added to the group
+
+  INSERT INTO users_groups (users_id, groups_id)
+  VALUES (2, 1);
+
+  INSERT INTO users_groups (users_id, groups_id)
+  VALUES (3, 1);
+
