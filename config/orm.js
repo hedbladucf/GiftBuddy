faddf44e8f596f9100b06a8711d618b507ec52ec
Michelle Hettinger <Michelle Hettinger>
2016-10-01 15:15:52 -0400

    added semicolon

--- config/orm.js
+++ config/orm.js
@@ -70,7 +70,7 @@ var orm = {
     },
 
     logOn: function(email, cb){
-        var queryString = "SELECT users_groups.id, users_groups.groups_id, users_groups.admin, groups.group_name, groups.dollar_amount, users.email FROM users_groups JOIN groups ON users_groups.groups_id = groups.id JOIN users ON users_groups.users_id = users.id WHERE users.email = (?)"
+        var queryString = "SELECT users_groups.id, users_groups.groups_id, users_groups.admin, groups.group_name, groups.dollar_amount, users.email FROM users_groups JOIN groups ON users_groups.groups_id = groups.id JOIN users ON users_groups.users_id = users.id WHERE users.email = (?)";
 
         console.log(queryString);
 
