e0f9a369c3a49d7455aa057bc13396c0c2aa2a00
Michelle Hettinger <Michelle Hettinger>
2016-10-01 16:09:09 -0400

    logging on now also grabs the users name from the database

--- config/orm.js
+++ config/orm.js
@@ -70,7 +70,7 @@ var orm = {
     },
 
     logOn: function(email, cb){
-        var queryString = "SELECT groups.g_id, users.u_id, groups.group_name, users_groups.admin, groups.dollar_amount, groups.active FROM users_groups JOIN groups ON users_groups.groups_id = groups.g_id JOIN users ON users_groups.users_id = users.u_id WHERE users.email = (?)";
+        var queryString = "SELECT groups.g_id, users.u_id, users.full_name, groups.group_name, users_groups.admin, groups.dollar_amount, groups.active FROM users_groups JOIN groups ON users_groups.groups_id = groups.g_id JOIN users ON users_groups.users_id = users.u_id WHERE users.email = (?)";
 
         console.log(queryString);
 
