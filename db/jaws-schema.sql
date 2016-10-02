971de6929e9b9fcb17dedfeb1e892280c1efd592
Michelle Hettinger <Michelle Hettinger>
2016-10-01 21:25:57 -0400

    cleaned up the schema to make it more visually appealing

--- db/jaws-schema.sql
+++ db/jaws-schema.sql
@@ -6,29 +6,29 @@ DROP TABLE `p2o40rwbj6zuxy26`.`groups`;
 use p2o40rwbj6zuxy26;
 
 CREATE TABLE `p2o40rwbj6zuxy26`.`users` (
-  `u_id` int(11) NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(99) NOT NULL,
-  `address` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `u_id` INT NOT NULL AUTO_INCREMENT,
+  `full_name` VARCHAR(99) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`u_id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   
-  CREATE TABLE `p2o40rwbj6zuxy26`.`groups` (
+CREATE TABLE `p2o40rwbj6zuxy26`.`groups` (
   `g_id` INT NOT NULL AUTO_INCREMENT,
   `group_name` VARCHAR(45) NOT NULL,
   `dollar_amount` INT NOT NULL,
   `active` TINYINT NOT NULL DEFAULT 1,
-  `createdAt` DATETIME DEFAULT current_timestamp,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`g_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  CREATE TABLE `p2o40rwbj6zuxy26`.`users_groups` (
+CREATE TABLE `p2o40rwbj6zuxy26`.`users_groups` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `users_id` INT NOT NULL,
   `groups_id` INT NOT NULL,
@@ -37,9 +37,9 @@ CREATE TABLE `p2o40rwbj6zuxy26`.`users` (
   `assigned_user_id` INT NOT NULL DEFAULT 0,
   `sent` TINYINT NOT NULL DEFAULT 0,
   `received` TINYINT NOT NULL DEFAULT 0,
-  `createdAt` DATETIME DEFAULT current_timestamp,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`users_id`) REFERENCES users(`u_id`),
   FOREIGN KEY (`groups_id`) REFERENCES groups(`g_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- db/schema.sql
+++ db/schema.sql
@@ -5,29 +5,29 @@ create database project_db;
 use project_db;
  
 CREATE TABLE `project_db`.`users` (
-  `u_id` int(11) NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(99) NOT NULL,
-  `address` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `u_id` INT NOT NULL AUTO_INCREMENT,
+  `full_name` VARCHAR(99) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`u_id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   
-  CREATE TABLE `project_db`.`groups` (
+CREATE TABLE `project_db`.`groups` (
   `g_id` INT NOT NULL AUTO_INCREMENT,
   `group_name` VARCHAR(45) NOT NULL,
   `dollar_amount` INT NOT NULL,
   `active` TINYINT NOT NULL DEFAULT 1,
-  `createdAt` DATETIME DEFAULT current_timestamp,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`g_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  CREATE TABLE `project_db`.`users_groups` (
+CREATE TABLE `project_db`.`users_groups` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `users_id` INT NOT NULL,
   `groups_id` INT NOT NULL,
@@ -36,9 +36,9 @@ CREATE TABLE `project_db`.`users` (
   `assigned_user_id` INT NOT NULL DEFAULT 0,
   `sent` TINYINT NOT NULL DEFAULT 0,
   `received` TINYINT NOT NULL DEFAULT 0,
-  `createdAt` DATETIME DEFAULT current_timestamp,
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`users_id`) REFERENCES users(`u_id`),
   FOREIGN KEY (`groups_id`) REFERENCES groups(`g_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
