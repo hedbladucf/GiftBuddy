8b3efe99ea7f3ecc9cf473d64d386efbac19f172
Michelle Hettinger <Michelle Hettinger>
2016-10-01 21:03:20 -0400

    added to users_groups table the following columns: item_note. added updatedAt columns to groups and users_groups

--- db/jaws-schema.sql
+++ db/jaws-schema.sql
@@ -24,17 +24,22 @@ CREATE TABLE `p2o40rwbj6zuxy26`.`users` (
   `dollar_amount` INT NOT NULL,
   `active` TINYINT NOT NULL DEFAULT 1,
   `createdAt` DATETIME DEFAULT current_timestamp,
-  PRIMARY KEY (`g_id`));
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`g_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   CREATE TABLE `p2o40rwbj6zuxy26`.`users_groups` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `users_id` INT NOT NULL,
   `groups_id` INT NOT NULL,
-  `assigned_user_id` INT NOT NULL DEFAULT 0,
   `admin` TINYINT NOT NULL DEFAULT 0,
+  `item_note` VARCHAR(200),
+  `assigned_user_id` INT NOT NULL DEFAULT 0,
   `sent` TINYINT NOT NULL DEFAULT 0,
   `received` TINYINT NOT NULL DEFAULT 0,
   `createdAt` DATETIME DEFAULT current_timestamp,
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`users_id`) REFERENCES users(`u_id`),
-  FOREIGN KEY (`groups_id`) REFERENCES groups(`g_id`));
+  FOREIGN KEY (`groups_id`) REFERENCES groups(`g_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
