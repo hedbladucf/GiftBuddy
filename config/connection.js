cdb63a2a16e17c6733c2afae094e4ff4d46939a5
Michelle Hettinger <Michelle Hettinger>
2016-10-13 21:21:03 -0400

    started send gift stuff

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL){
      connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 's0nyd0g',
         database: 'project_db'
     });
 }
